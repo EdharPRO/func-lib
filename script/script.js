4200a93262ea3c8974c20c0da1f52fe50c8fd422
Edhar Prokudin <prokudin.ed@gmail.com>
2021-07-09 23:32:14 +0300

    first

---
+++ script/script.js
@@ -0,0 +1,11 @@
+const fruts = ['apple', 'banana', 'cherry', 'pineapple'];
+const girlNames = ['Olia','Ania','Vika','Alina'];
+const students = ['Sasha', 'Maxim', 'Oleg', 'Kostia'];
+const things = ['oil', 'suitcase', 'ticket', 'wallet'];
+const numbers = [1,2,3,4,5,6];
+const bigNames = ['ALINA', 'VIKA','ANIA', 'OLIA'];
+let users = [
+    {id: 1, name: "Вася"},
+    {id: 2, name: "Петя"},
+    {id: 3, name: "Маша"}
+];
