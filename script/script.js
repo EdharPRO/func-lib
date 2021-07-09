83f2d23b25d37c798c7bfc67654653b6419ec0f9
Edhar Prokudin <prokudin.ed@gmail.com>
2021-07-09 23:37:04 +0300

    forEach

--- script/script.js
+++ script/script.js
@@ -9,3 +9,23 @@ let users = [
     {id: 2, name: "Петя"},
     {id: 3, name: "Маша"}
 ];
+
+
+girlNames.forEach(function olo(name) {
+    console.log(name)
+});
+
+fruts.forEach((frut) => console.log(frut)); 
+
+
+function forEach(arr, callback) {
+    for(let i = 0; i < arr.length; i++) {
+        callback(arr[i], i, 'студент')
+    };
+};
+
+function callBackss(arr, index, hello) {
+    console.log(arr, index, hello)
+}
+
+forEach(students, callBackss);
